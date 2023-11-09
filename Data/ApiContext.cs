2c5eef5542e59804d1ba33912701cb94c53b67b5
Brunon Lemański <brul@netcompany.com>
2023-11-09 10:34:23 +0100

    added api context

---
+++ Data/ApiContext.cs
@@ -0,0 +1,11 @@
+﻿using eict_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eict_api.Data
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<PackageType> PackageTypes { get; set; }
+
+    }
+}
---
+++ Models/PackageType.cs
@@ -0,0 +1,9 @@
+﻿namespace eict_api.Models
+{
+    public class PackageType
+    {
+        public int id { get; set; }
+        public string type { get; set; }
+        public float fee { get; set; }
+    }
+}
